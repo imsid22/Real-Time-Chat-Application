9382342db16c38a6e49786cefbbc569188543528
imsid22 <siddhantd94@gmail.com>
2023-08-19 04:43:41 -0600

    feat: Set up the initial database connection (MongoDB)

--- server.js
+++ server.js
@@ -18,3 +18,35 @@ app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 });
 
+// Setting up the database connection (MongoDB)
+
+mongoose.connect('mongodb://localhost:27017/chatapp', {
+    newUseUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Handling the connection events
+const db = mongoose.connection
+// In case of an error
+db.on('error', console.error.bind(console, 'connection error:'));
+// When connection is successful
+db.once('open', function() {
+    console.log('Connection to MongoDB successful');
+});
+
+// Generate a schema for the database
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+// Setting up the message schema
+const messageSchema = new mongoose.Schema({
+    content: String,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    createdAt: {type: Date, default: Date.now}
+});
+
+// Create models for the schemas
+const User = mongoose.model('User', userSchema);
+const Message = mongoose.model('Message', messageSchema);

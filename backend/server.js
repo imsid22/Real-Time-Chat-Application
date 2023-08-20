3211cc71e27a9c63d46a2e65d7064b89d6f3ae65
imsid22 <siddhantd94@gmail.com>
2023-08-19 19:02:10 -0600

    feat: Set up the initial WebSocket server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const websocket = require('ws');
+const http = require('http');
 
 // Initialize the express application
 const app = express();
@@ -50,3 +51,38 @@ const messageSchema = new mongoose.Schema({
 // Create models for the schemas
 const User = mongoose.model('User', userSchema);
 const Message = mongoose.model('Message', messageSchema);
+
+// Setting up the websocket server
+
+// Creating an HTTP server
+const server = http.createServer(app);
+const wss = new websocket.Server({server});
+
+// WebSocket Listner
+wss.on('connection', (ws) => {
+    console.log('Client Connected');
+
+    // When a message is received
+    wss.on('message', (message) => {
+        console.log(`Received ${message}`);
+        console.log('Received: ', message);
+
+        // Broadcast the message to all the clients
+        wss.clients.forEach((client) => {
+            if (wss != client && client.readyState == websocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    // On client disconnection
+    wss.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server started on, http://localhost:${PORT}`);
+});
+
